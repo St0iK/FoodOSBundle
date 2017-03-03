0173b9f37e55ce9ba1039009989e3d8e04494e84
Dimitris Stoikidis <dimitris.stoikidis@creode.co.uk>
2017-03-03 16:32:05 +0000

    Adds second login form for adminstrators

--- src/St0iK/FoodosBundle/Controller/Admin/SecurityController.php
+++ src/St0iK/FoodosBundle/Controller/Admin/SecurityController.php
@@ -1,24 +1,59 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: jimstoik13
- * Date: 01/03/2017
- * Time: 14:24
- */
-
 namespace St0iK\FoodosBundle\Controller\Admin;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Security;
 
 
 class SecurityController extends  Controller
 {
-    /**
-     * @Route("/foodosbackend/login", name="foodosbackend_login")
-     */
+
     public function loginAction(Request $request)
     {
-        echo "xaxaxa";exit;
+        /** @var $session \Symfony\Component\HttpFoundation\Session\Session */
+        $session = $request->getSession();
+
+        $authErrorKey = Security::AUTHENTICATION_ERROR;
+        $lastUsernameKey = Security::LAST_USERNAME;
+
+        // get the error if any (works with forward and redirect -- see below)
+        if ($request->attributes->has($authErrorKey)) {
+            $error = $request->attributes->get($authErrorKey);
+        } elseif (null !== $session && $session->has($authErrorKey)) {
+            $error = $session->get($authErrorKey);
+            $session->remove($authErrorKey);
+        } else {
+            $error = null;
+        }
+
+        if (!$error instanceof AuthenticationException) {
+            $error = null; // The value does not come from the security component.
+        }
+
+        // last username entered by the user
+        $lastUsername = (null === $session) ? '' : $session->get($lastUsernameKey);
+
+        $csrfToken = $this->has('security.csrf.token_manager')
+            ? $this->get('security.csrf.token_manager')->getToken('authenticate')->getValue()
+            : null;
+
+        return $this->renderLogin(array(
+            'last_username' => $lastUsername,
+            'error' => $error,
+            'csrf_token' => $csrfToken,
+        ));
+    }
+
+    protected function renderLogin(array $data)
+    {
+        return $this->render('@FOSUser/Security/login.html.twig', $data);
+    }
+
+    public function loginCheckAction()
+    {
+        return new Response('', 400);
     }
 }
