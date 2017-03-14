c48956fe08d2ff0eb8aeea25bc96bb7c0142f312
Dimitris Stoikidis <dimitris.stoikidis@creode.co.uk>
2017-03-14 11:13:39 +0000

    Update admin template on indexAction

--- src/St0iK/FoodosBundle/Controller/Admin/DefaultController.php
+++ src/St0iK/FoodosBundle/Controller/Admin/DefaultController.php
@@ -14,6 +14,6 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
-        return $this->render('@Foodos/Default/index.html.twig');
+        return $this->render('@Foodos/Admin/Pages/index.html.twig');
     }
 }
