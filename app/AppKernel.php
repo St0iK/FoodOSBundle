43abee75c6ecd422a5914ca71f8d07ea8e3d61a0
Dimitris Stoikidis <dimitris.stoikidis@creode.co.uk>
2017-03-14 12:04:41 +0000

    Add DoctrineMigrations to Kernel

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new St0iK\FoodosBundle\FoodosBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
+            new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
